e35cd88eb32df3a0872dcf03955c211448e2721d
Jorge Laguna <jld0016@alu.ubu.es>
2018-04-10 19:29:14 +0200

    Se ha vuelto a generar la solución, debido a una perdida de ciertos archivos de configuración del anterior proyecto creado inicialmente.
    La configuración es la misma.

---
+++ NYHApp/NYHApp/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace NYHApp.Models
+{
+    [Table("Users")]
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Surname1 { get; set; }
+
+        public string Surname2 { get; set; }
+
+        public string Address { get; set; }
+    }
+}
